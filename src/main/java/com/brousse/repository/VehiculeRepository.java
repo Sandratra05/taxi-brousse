6878ab9c6a56844ef57b81c386cc480733484756
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-01-10 22:55:30 +0300

    modification fonction

--- src/main/java/com/brousse/repository/VehiculeRepository.java
+++ src/main/java/com/brousse/repository/VehiculeRepository.java
@@ -19,12 +19,19 @@ public interface VehiculeRepository extends JpaRepository<Vehicule, Integer> {
     List<Vehicule> findByCurrentStatutId(@Param("statutId") Integer statutId);
 
     @Query(value = """
-        SELECT v.id, v.immatriculation, v.modele, (b.montant_total * pv.nb_place) AS chiffreAffaire
+                SELECT
+            v.id_vehicule,
+            v.immatriculation,
+            v.modele,
+            SUM(b.montant_total * pv.nb_place) AS chiffreAffaire
         FROM vehicule v
         JOIN voyage vo ON v.id_vehicule = vo.id_vehicule
         JOIN billet b ON vo.id_voyage = b.id_voyage
-        JOIN client c ON b.id_client = c.id_client
         JOIN place_vehicule pv ON v.id_place_vehicule = pv.id_place_vehicule
+        GROUP BY
+            v.id_vehicule,
+            v.immatriculation,
+            v.modele;
         """, nativeQuery = true)
     List<Object[]> findChiffreAffaireParVehicule();
 }
